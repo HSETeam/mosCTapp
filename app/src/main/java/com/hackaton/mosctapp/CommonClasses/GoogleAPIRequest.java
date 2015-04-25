7b9ba9abe1614d8df9df1585891864770a4b2f0f
Artemiy Shlesberg <denis@Denis-MBP.local>
2015-04-26 01:29:44 +0300

    api test

--- app/src/main/java/com/hackaton/mosctapp/CommonClasses/GoogleAPIRequest.java
+++ app/src/main/java/com/hackaton/mosctapp/CommonClasses/GoogleAPIRequest.java
@@ -10,23 +10,28 @@ import org.apache.http.Header;
 
 public class GoogleAPIRequest {
 
-    receiveDistance delegate;
+    receiveDistance listener;
+
     void getRouteDistance (float lon1, float lat1, float lon2, float lat2){
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
+        params.put("origin", lon1+","+lat1);
+        params.put("destination", lon2+","+lat2);
+        params.put("sensor", "true");
+        params.put("mode","walking");
+
         client.get("http://maps.googleapis.com/maps/api/directions/output?parameters\n",params,new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 //TODO parse answer
                 System.out.println("answer received");
-                Log.d("pizda", "answer received");
-                delegate.distanceReceived(0);
+                Log.d("pizda", "answer received\n"+(new String(responseBody)));
+                listener.distanceReceived(0);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                System.out.println("answer did not received");
-                Log.d("pizda","answer did not received");
+                Log.d("pizda","answer did not received\n"+(new String(responseBody)));
             }
         });
     }
